c3cf9bdfdcf990e6bb9bbb942a0910d2b1ca7046
Alexandru Corut <r3dm4n@me.com>
2017-05-06 12:03:10 +0300

    UI

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,26 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by r3d on 06/05/2017.
+//  Copyright © 2017 r3d. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
