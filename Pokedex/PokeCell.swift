597659ea748cd5be5b8844f3370149c83910e175
Alexandru Corut <r3dm4n@me.com>
2017-05-06 13:33:07 +0300

    Collection delegate, data source, flow layout

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -14,12 +14,19 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var nameLbl: UILabel!
     
     var pokemon: Pokemon!
+   
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5
+    }
     
-    func configureCell(pokemon: Pokemon) {
+    func configureCell(_ pokemon: Pokemon) {
         self.pokemon = pokemon
         
         nameLbl.text = self.pokemon.name.capitalized
         thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
         
     }
 
