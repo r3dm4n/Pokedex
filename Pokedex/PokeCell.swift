8f5ebdad39d6cdd9e5e80fee723849559822bfff
Alexandru Corut <r3dm4n@me.com>
2017-05-06 12:16:51 +0300

    Custom collection view cell

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by r3d on 06/05/2017.
+//  Copyright © 2017 r3d. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
     }
 
  
